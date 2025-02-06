d5f69d7db80294052afb517e95826b012168e4cb
Sami Rollins <srollins@gmail.com>
2025-02-06 14:24:39 -0800

    add Driver template

---
+++ src/main/java/FitnessTracker.java
@@ -0,0 +1,10 @@
+/**
+ * Required driver class for the FitnessTracker program.
+ */
+public class FitnessTracker {
+
+    public static void main(String[] args) {
+        // TODO: YOUR CODE HERE!
+    }
+
+}
